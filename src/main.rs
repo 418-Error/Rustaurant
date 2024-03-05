6bff5b1765d1b41d0789391a9c317c2dcc250c6c
mathias <mathiasdurat@gmail.com>
2024-03-05 18:20:33 +0100

    parsing finished data fully parsed

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 // use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
 use std::{error::Error, process};
+use bson::RawArray;
+use csv::StringRecord;
+// use csv::StringRecord;
 use mongodb::bson::doc;
 // use dotenv::dotenv;
 // use tokio;
+
+// #[derive(Debug, Deserialize)]
+// struct OpeningHour {
+//     opening_hour: Option<Time>,
+//     closing_hour: Option<Time>,
+// }
+
+// #[derive(Debug, Deserialize)]
+// struct OpeningHours {
+//     monday: Option<OpeningHour>,
+//     tuesday: Option<OpeningHour>,
+//     wednesday: Option<OpeningHour>,
+//     thursday: Option<OpeningHour>,
+//     friday: Option<OpeningHour>,
+//     saturday: Option<OpeningHour>,
+//     sunday: Option<OpeningHour>,
+// }
 
 // #[derive(Debug, Deserialize)]
 // struct Record {
@@ -16,14 +36,14 @@ use mongodb::bson::doc;
 //     ref: Option<u64>,
 //     wheelchair: bool,
 //     internet_access: bool,
-//     opening_hours: Option<tim>,
-//     drive_through,
-//     building,
-//     cuisine,
-//     capacity,
-//     tourism,
-//     osm_user,
-//     osm_timestamp,
+//     opening_hours: Option<OpeningHours>,
+//     drive_through: Option<bool>,
+//     building: Option<String>,
+//     cuisine: Option<[String]>,
+//     capacity: Option<u64>,
+//     tourism: Option<String>,
+//     osm_user: Option<String>,
+//     osm_timestamp: Option<String>,
 //     tags,
 // }
 
@@ -34,12 +54,67 @@ fn read_csv() -> Result<(), Box<dyn Error>> {
         // The iterator yields Result<StringRecord, Error>, so we check the
         // error here..
         let mut doc = doc!{};
+        let mut previous_key = "".to_string();
+        let mut concat: Vec<String> = Vec::new();
         let record = result?;
-        for (i) in record[18].rsplit(", "){
+        for i in record[18].rsplit(", "){
             let elem: Vec<&str>= i.split("=>").collect();
-            doc.insert(elem[0], elem[1]);
+            let is_good = match elem.len() {
+                2 => true,
+                _ => false,
+            };
+            if is_good {
+                if concat.len() > 0 {
+                    let split: Vec<&str> = previous_key.split(":").collect();
+                    if split.len() == 2 {
+                        let last = doc.get(split[0].replace("\"",""));
+                        if last.is_some() {
+                            let ancient = last.unwrap();
+                            let new = ancient.as_document();
+                            if !new.is_none() {
+                                let mut new = new.unwrap().clone();
+                                new.insert(split[1].replace("\"",""), concat);
+                                doc.insert(split[0].replace("\"",""), new);
+                                concat = Vec::new();
+                                }
+                        }
+                        else{
+                            doc.insert(split[0].replace("\"",""), doc!{split[1].replace("\"",""): concat});
+                            concat = Vec::new();
+                        }
+                    }
+                    else {
+                        doc.insert(previous_key, concat);
+                        concat = Vec::new();
+                    }
+                }
+                let split: Vec<&str> = elem[0].split(":").collect();
+                if split.len() == 2 {
+                    let last = doc.get(split[0].replace("\"",""));
+                    if last.is_some() {
+                        let ancient = last.unwrap();
+                        let new = ancient.as_document();
+                        if !new.is_none() {
+                            let mut new = new.unwrap().clone();
+                            new.insert(split[1].replace("\"",""), elem[1].replace("\"",""));
+                            doc.insert(split[0].replace("\"",""), new);
+                            previous_key = elem[0].replace("\"", "");
+                            continue;
+                        }
+                    }
+                    doc.insert(split[0].replace("\"",""), doc!{split[1].replace("\"",""): elem[1].replace("\"","")});
+                    previous_key = elem[0].replace("\"", "");
+                    continue;
+                }
+                doc.insert(elem[0].replace("\"",""), elem[1].replace("\"",""));
+                previous_key = elem[0].replace("\"", "");
+            }
+            else {
+                let inter = elem[0].replace("\"", "");
+                concat.push(inter);
+            }
         }
-        println!("{:?}", doc.get("osm_id"));
+        println!("{:?}", doc);
     }
     Ok(())
 }
